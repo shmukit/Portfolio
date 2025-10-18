c5e4cd3ed1ccf963b0f0ff862a028d21d4b2313c
shmukit <mukitshazzadhossain@gmail.com>
2025-10-18 23:41:22 +0600

    Speed up mobile modal transitions: increase spring stiffness and reduce exit duration

--- lib/utils/animations.ts
+++ lib/utils/animations.ts
@@ -423,8 +423,8 @@ export const mobileModalSlide: Variants = {
     scale: 1,
     transition: {
       type: "spring",
-      stiffness: 300,
-      damping: 30
+      stiffness: 500,
+      damping: 35
     }
   },
   exit: {
@@ -432,7 +432,7 @@ export const mobileModalSlide: Variants = {
     opacity: 0,
     scale: 0.95,
     transition: { 
-      duration: 0.3, 
+      duration: 0.2, 
       ease: "easeIn" 
     }
   }
